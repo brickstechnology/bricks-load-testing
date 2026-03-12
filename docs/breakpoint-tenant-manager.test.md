200544bb86493c962784435b050c9aaab8b0fa37
nack098 <tapaneeya@windowslive.com>
2026-03-12 17:39:57 +0700

    Initialize Project

---
+++ docs/breakpoint-tenant-manager.test.md
@@ -0,0 +1,33 @@
+---
+created-at: 12 March 2026
+updated-at: 12 March 2026
+version: 0.1.0
+---
+
+# Tenant Manager Breakpoint Testing
+
+## Revision Information
+| Version | Date | Amendment | Author |
+| 0.1.0 | 12 March 2026 | Initial Breakpoint Testing Plan | Tapaneeya Odmung |
+
+## BP01 - Tenant Manager Maximum Concurrent Request Limit
+Description: Finding maximum number of concurrent request of tenant manager before system breakdown.
+Endpoint: [https://api-vcluster-dev.staging.internal.aws.brickstech.co/api/v1]
+Type: GET
+Duration: 30 min
+Steps:
+    1. Load Authorization Token
+    2. Load Tenant ID
+    3. Load Tenant Provider
+    4. Setting Header
+        4.1. Authorization as Bearer ${Authorization Token}
+        4.2. X-Tenant-ID as ${Tenant ID}
+        4.3. X-Tenant-Provider as ${Tenant Provider}
+    5. Set concurrent request as 10 requests
+    6. Send HTTP GET request to the Endpoint
+    7. Check if requests taking longer than 1 seconds or HTTP response does not within range of 200 - 299  (inclusive)
+        7.1: If Success Rate = 100% AND Latency < 1s, increase load.
+        7.2: If Success Rate < 95% OR Latency > 1s for more than 30 seconds, mark this as the Breakpoint and begin the "Scale Down" phase.
+Expected Outcome:
+    - Maximum concurrent request before system breakdown. 
+    - CPU and Memory usage of a system.
---
+++ tests/pre/stress-tenant-manager.test.ts
@@ -0,0 +1,55 @@
+import { check, sleep } from "k6";
+import { type Options, type Stage } from "k6/options";
+import http from "k6/http";
+
+const BASE_URL = __ENV.BASE_URL;
+if (!BASE_URL) {
+  throw new Error("ERROR: BASE_URL is not set. Please check your environment variables.");
+}
+
+function createStressStages(): Stage[] {
+  const stressCount = parseInt(__ENV.STRESS_COUNT || "0", 10);
+  const steps = Math.floor(stressCount / 10);
+  const stages: Stage[] = [];
+
+  if (steps === 0) return stages;
+
+  for (let i = 1; i <= steps; ++i)
+    stages.push({ duration: "10m", target: i * 10 });
+
+  stages.push({ duration: "30m", target: stressCount });
+
+  for (let i = steps - 1; i >= 0; --i)
+    stages.push({ duration: "10m", target: i * 10 });
+
+  return stages;
+}
+
+export const options: Options = {
+  stages: createStressStages(),
+  thresholds: {
+    http_req_failed: ['rate < 0.01'], // 99 percent success rate
+    http_req_duration: ['p(95) < 1000'] // 95 percent of request should have less than 1 seconds latency
+  }
+}
+
+export default function() {
+  const BASE_URL = __ENV.BASE_URL;
+  if (!BASE_URL) {
+    console.log("ERROR: BASE_URL is not set. Please check you environment variable.");
+    return;
+  }
+
+  const res = http.get(BASE_URL, {
+    headers: {
+      "Authorization": `Bearer ${__ENV.TOKEN}`,
+      "X-Tenant-ID": __ENV.TENANT_ID || "",
+      "X-Tenant-Provider": __ENV.TENANT_PROVIDER || "",
+    }
+  });
+
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  });
+  sleep(1);
+}
